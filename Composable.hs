2ba1ded9c1450103cd7e1a43c627afda045fb2a5
nickkuk <nickkuklin@gmail.com>
2017-07-02 10:49:16 +0500

    First attempt to use Transient.

--- Composable.hs
+++ Composable.hs
@@ -1,14 +1,45 @@
 module Main where
 
-import Control.Monad.Trans.State
+import Control.Applicative
 import Control.Monad
 import Control.Monad.IO.Class
-import Foreign.C.Types (CInt)
-import Data.Word
+import Control.Concurrent
 import SDL
 import Transient.Base
-import Transient.Indeterminism
 import Transient.Internals
 
+newtype NeedRedraw = NeedRedraw Bool
+
+redraw :: TransIO ()
+redraw = do
+  NeedRedraw needRedraw <- getState
+  when needRedraw $ do
+    renderer <- getState
+    rendererDrawColor renderer $= V4 255 255 255 255
+    clear renderer
+    present renderer
+    setState $ NeedRedraw False
+
 main :: IO ()
-main = putStrLn "Hello, World!"
+main = void . runTransient . threads 0 $ do              -- run TransIO in initial (bounded) thread
+  initSDL
+  setState $ NeedRedraw True
+  redraw
+  e <- waitEvents SDL.waitEvent
+  exitSDL e <|> redraw
+  return ()
+
+initSDL :: TransIO ()
+initSDL = do
+  SDL.initialize [InitVideo]
+  window <- createWindow "Composable" defaultWindow
+  setState window
+  createRenderer window (-1) defaultRenderer >>= setState
+
+exitSDL :: Event -> TransIO ()
+exitSDL (Event _ QuitEvent) = do
+  getState >>= destroyRenderer
+  getState >>= destroyWindow
+  SDL.quit
+  liftIO $ myThreadId >>= killThread                     -- how to stop listening events?
+exitSDL _ = stop
